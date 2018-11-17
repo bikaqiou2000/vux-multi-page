2ae8275c323c75b386be413159f01fc4a1f8aeec
bikaqiou <bikaqiou2000@sina.com>
2018-11-17 16:46:27 +0800

    wip  common msg  page

---
+++ src/pages/CommonMsg/commonmsg.vue
@@ -0,0 +1,158 @@
+<template>
+  <div style="height:100%;">
+    <view-box ref="viewBox" :body-padding-top="46">
+      <x-header slot="header">
+        二工厂-业务消息
+      </x-header>
+      <div>
+        
+        <button-tab style="padding:12px" >
+          <button-tab-item selected>未读消息</button-tab-item>
+          <button-tab-item>已读消息</button-tab-item>
+        </button-tab>
+
+        <group>
+          <calendar v-model="demo2" title="查看日期" disable-future></calendar>
+        </group>
+
+        <card style="padding:12px" v-for="msgitem in list" :key="msgitem.id">
+          <div slot="content">
+            <flexbox>
+              <flexbox-item :span="6"><p class="msg-title">{{msgitem.title}}</p></flexbox-item>
+            </flexbox>
+            <flexbox>
+              <flexbox-item :span="2" ><p class="msg-field-head">内容</p></flexbox-item>
+              <flexbox-item><p class="msg-field-content">{{msgitem.desc}}</p></flexbox-item>
+            </flexbox>
+            <flexbox>
+              <flexbox-item :span="2"><p class="msg-field-head">时间</p></flexbox-item>
+              <flexbox-item><p class="msg-field-content">{{msgitem.date}}</p></flexbox-item>
+            </flexbox>
+            <flexbox>
+              <flexbox-item :span="2"><p class="msg-field-head">已读</p></flexbox-item>
+              <flexbox-item><check-icon :value.sync="msgitem.isread"></check-icon></flexbox-item>
+            </flexbox>
+          </div>
+        </card>
+        <!--
+        <panel header="消息列表" :list="list" type="4" ></panel>
+        -->
+      </div>
+    </view-box>
+  </div>
+</template>
+
+<script>
+import { 
+  XHeader, XButton, Box, Flexbox, FlexboxItem, 
+  Grid, GridItem, ButtonTab, ButtonTabItem,Badge,Calendar,
+  Panel, Group, ViewBox, Card, CheckIcon
+} from 'vux'
+
+
+export default {
+   components: {
+    XHeader, XButton, Box, Flexbox, FlexboxItem, 
+    Grid, GridItem, ButtonTab, ButtonTabItem,Badge,Calendar,
+    Panel,Group, ViewBox, Card, CheckIcon 
+   },
+  name: 'app',
+  data () {
+    return {
+      demo2: 'TODAY',
+      list: [{
+        id:1,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:true,
+      }, {
+        id:2,
+        title: '694803归队队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      {
+        id:3,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      {
+        id:4,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      {
+        id:5,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      {
+        id:6,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      {
+        id:7,
+        title: '694803离队',
+        desc: '车型：E5 配置：CBD 点位：PC1 离队原因：物料1。',
+        date: '2018-10-01 23:20:00',
+        isread:false,
+      },
+      ],
+    }
+  },
+  methods: {
+    processButton001 () {
+        //console.log("click +1!")
+    }
+  }
+}
+</script>
+
+<style lang="less" >
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+  background-color: #fbf9fe;
+}
+
+group-title{
+  color: rgb(128, 127, 127);
+}
+.weui-grids {
+  background-color: #fff;
+}
+.badge-value {
+  display: inline-block!important;
+}
+.vertical-middle {
+  vertical-align: middle;
+}
+.msg-title{
+  padding: 5px;
+  font-weight:800;
+  color: rgb(128, 127, 127);
+}
+.msg-field-head{
+  padding: 2px;
+  color: rgb(128, 127, 127);
+}
+.msg-field-content{
+  padding: 2px;
+  color: rgb(128, 127, 127);
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>
-    <x-header :left-options="{showBack: false}">海马生产协同-主页</x-header>
+  <div style="height:100%;" >
+    <view-box ref="viewBox" :body-padding-top="46">
+    <x-header :left-options="{showBack: false}">海马MES-主页</x-header>
     <div style="padding: 15px;">
-      <button-tab v-model="demo01">
+      <button-tab >
         <button-tab-item selected>二工厂</button-tab-item>
         <button-tab-item>三工厂</button-tab-item>
       </button-tab>
       <br/>
-
       <group-title>消息通知</group-title>
       <grid  :cols="2"  :show-lr-borders="true" :show-vertical-dividers="false">
-        <grid-item :label="最新通知">
+        <grid-item :lable="'最新通知'">
           <img slot="icon" src="../../assets/msg.png">
           <span slot="label">
             最新通知
             <badge text="8"></badge>
           </span>
         </grid-item>
-        <grid-item :label="最新警告">
+        <grid-item >
           <img slot="icon" src="../../assets/msgsystem.png">
           <span slot="label">
             最新警告
@@ -27,64 +27,64 @@
       </grid>
       <br/>
       <group-title>管理数据</group-title>
-      <grid :cols="2" :show-lr-borders="true" :show-vertical-dividers="false">
-        <grid-item :label="我的消息">
+      <grid :cols="2" :show-lr-borders="false" :show-vertical-dividers="false">
+        <grid-item >
           <img slot="icon" src="../../assets/data06.png">
           <span slot="label">实时产量</span>
         </grid-item>
-        <grid-item :label="车辆信息">
-          <img slot="icon" src="../../assets/database.png">
+        <grid-item >
+          <img slot="icon" src="../../assets/car.png">
           <span slot="label">车辆信息</span>
         </grid-item>
-        <grid-item :label="过点信息">
+        <grid-item >
           <img slot="icon" src="../../assets/database.png">
           <span slot="label">过点信息</span>
         </grid-item>
       </grid>
     </div>
+    </view-box>
   </div>
 </template>
 
 <script>
-import { 
-  XHeader, XButton, Box, Flexbox, FlexboxItem, 
-  Grid, GridItem, ButtonTab, ButtonTabItem,Badge,
-} from 'vux'
-
+import {
+  XHeader,XButton,Box,Flexbox,FlexboxItem,
+  Grid,GridItem,ButtonTab,ButtonTabItem,
+  Badge, ViewBox, Group, GroupTitle,
+} from "vux";
 
 export default {
-   components: {
-    XHeader,XButton,Box,Flexbox,
-    FlexboxItem,Grid,GridItem,
-    ButtonTab, ButtonTabItem,Badge
-   },
-  name: 'app',
-  data () {
-    return {
-    }
+  components: {
+    XHeader,XButton,Box,Flexbox,FlexboxItem,
+    Grid,GridItem,ButtonTab,ButtonTabItem,
+    Badge, ViewBox, Group, GroupTitle, 
+  },
+  name: "app",
+  data() {
+    return {};
   },
   methods: {
-    processButton001 () {
-        console.log("click +1!")
+    processButton001() {
+      console.log("click +1!");
     }
   }
-}
+};
 </script>
 
 <style lang="less" >
-@import '~vux/src/styles/reset.less';
-@import '~vux/src/styles/1px.less';
+@import "~vux/src/styles/reset.less";
+@import "~vux/src/styles/1px.less";
 body {
   background-color: #fbf9fe;
 }
-group-title{
+group-title {
   color: gray;
 }
 .weui-grids {
   background-color: #fff;
 }
 .badge-value {
-  display: inline-block!important;
+  display: inline-block !important;
 }
 .vertical-middle {
   vertical-align: middle;
